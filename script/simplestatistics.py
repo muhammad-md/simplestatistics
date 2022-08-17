428b768589d4f96fd7b5530f4ecb7e5e8d4b6865
Muhammad <mammanali8228@gmail.com>
2022-08-17 22:08:36 +0100

    added Multiple linear regression

--- script/simplestatistics.py
+++ script/simplestatistics.py
@@ -91,7 +91,7 @@ class mainwindow:
         self.btn4.destroy()
         self.btn5.destroy()
         self.btn6.destroy()
-        self.another = Piechart(self.master)
+        self.another = multiplelinearreg(self.master)
 
 #One way anova component
 class Oneway:
@@ -498,6 +498,149 @@ class simplelinearreg:
                     y = list(columndependent_var)
 
                     x = np.array(x).reshape((-1, 1))
+                    y = np.array(y)
+
+                    x = sm.add_constant(x)
+                    model = sm.OLS(y, x)
+                    result = model.fit()
+                    summary = result.summary()
+                    print(result.summary())
+
+                    model2 = LinearRegression().fit(x, y)
+                    interc = model2.intercept_
+                    print("Intercept: ", interc)
+
+                    slop = model2.coef_
+                    print("Slope: ", slop)
+
+                    y_pred = model2.predict(x)
+                    print("predicted response: ", y_pred)
+
+                    #setting up newwindow to embed the result on
+                    newwindow = Toplevel()      #TOPLEVEL() USED
+                    newwindow.title("RESULT WINDOW - ONE WAY ANOVA")
+                    newwindow.geometry("700x500")
+
+                    #setting space to view result on inside of the new window
+                    aovspace = tk.StringVar()
+                    aovspace2 = tk.StringVar()
+                    aovspace.set(summary)
+                    aovspace2.set(y_pred)
+
+                    self.label4= Label(newwindow, text="", textvariable=aovspace, font = fontStyle)
+                    self.label4.place(relx = 0.5, rely = 0.25, anchor = CENTER)
+
+                    self.label99= Label(newwindow, text="", textvariable=aovspace2, font = fontStyle)
+                    self.label99.place(relx = 0.5, rely = 0.60, anchor = CENTER)
+
+                    #Run new window
+                    newwindow.mainloop()
+        #def boxplot():
+         #   dependent_var = (self.entry_1.get())
+          #  independent_var = (self.entry_2.get())
+           # for file in excelfile:
+            #    data = pd.read_excel(file)
+             #   data.boxplot('%s' %(dependent_var), by='%s' %(independent_var) )
+              #  plt.show()
+                
+        #settinf up space to view the name of selected file
+        selectedfilespace = tk.StringVar()
+        
+        #function for selecting excel file to be used in the analysis
+        def select_file():
+            filetypes = (('text files', '*.xlsx'),('All files', '*.*'))
+            filename = fd.askopenfilename(title='Open a file',initialdir='/',filetypes=filetypes)
+
+            #send the selected excel file to the excelfile list
+            excelfile.append(filename)
+
+            #split and remove the path of the selected file and be left with that main filename
+            head, tail = os.path.split(filename)
+            mainfilename = tail
+            selectedfilespace.set(mainfilename)
+
+        #Buttons
+        self.label5= Label(self.master, text="File Selected: ", textvariable=selectedfilespace, font = fontStyle)
+        self.label5.place(relx = 0.5, rely = 0.15, anchor = CENTER)
+        #creating a checkbox to allow view boxplot
+        #self.btn5 = tk.Checkbutton(self.master, text="show boxplot", command=boxplot, font=fontStyle)
+        #self.btn5.place(relx = 0.5, rely = 0.33  , anchor = CENTER)
+        self.btn6 = tk.Button(self.master,text='Open a File',font= fontStyle1, command=select_file)
+        self.btn6.place(relx = 0.5, rely = 0.07, anchor = CENTER)
+        self.btn7 = tk.Button(self.master,text='Analyse',font= fontStyle1, command=analyse)
+        self.btn7.place(relx = 0.5, rely = 0.40, anchor = CENTER)
+                        
+        #function to clear the current page to view the previous page elements
+    def load_back(self):
+        self.entry_1.destroy()
+      #  self.entry_2.destroy()
+       # self.label1.destroy()
+        #self.label2.destroy()
+        #self.label3.destroy()
+        #self.label5.destroy()
+        #self.btn4.destroy()
+        #self.btn5.destroy()
+        #self.btn6.destroy()
+        #self.btn7.destroy()
+        #excelfile.clear()
+        #self.another = mainwindow(self.master)
+
+class multiplelinearreg:
+    def __init__(self, master):
+        # keep `root` in `self.master`
+        self.master = master
+        fontStyle1 = tkFont.Font(family="Lucida Grande", size=13)
+
+        #back button to view previous page, that is the main page
+        self.btn4 = tk.Button(self.master, text ="BACK", font = fontStyle1, command = self.load_back)
+        self.btn4.place(relx = 0.90, rely = 0.84, anchor = CENTER)
+        
+        self.label1 = Label(self.master, text="File Selected:", font = fontStyle)
+        self.label1.place(relx = 0.3, rely = 0.15, anchor = CENTER)
+        
+        self.label2 = Label(self.master, text="Dependent Variable: ", font = fontStyle)
+        self.label2.place(relx = 0.3, rely = 0.20, anchor = CENTER)
+        self.label3= Label(self.master, text="Independent Variable one : ", font = fontStyle)
+        self.label3.place(relx = 0.3, rely = 0.25, anchor = CENTER)
+        self.label4= Label(self.master, text="Independent Variable two : ", font = fontStyle)
+        self.label4.place(relx = 0.3, rely = 0.30, anchor = CENTER)
+
+        #entry box for dependent and independent variavles
+        self.entry_1 = Entry(self.master)
+        self.entry_2 = Entry(self.master)
+        self.entry_3 = Entry(self.master)
+        self.entry_1.place(relx = 0.5, rely = 0.20, anchor = CENTER)
+        self.entry_2.place(relx = 0.5, rely = 0.25, anchor = CENTER)
+        self.entry_3.place(relx = 0.5, rely = 0.30, anchor = CENTER)
+
+        #function for all the one way anova analysis   
+        def analyse():
+            #Check if a file selected whenever the user clicks "Analyse" button and showinformation if there is no file selected
+            if len(excelfile) == 0:
+                messagebox.showinfo("showinfo", "Please, select a file")
+            #Continue with the analysis if a file is selected
+            else:
+                dependent_var = (self.entry_1.get())
+                independent_var1 = (self.entry_2.get())
+                independent_var2 = (self.entry_3.get())
+                for file in excelfile:
+                    data = pd.read_excel(file)
+                    
+                    #getting the values in the selected columns
+                    columnindependent_var1 = data['%s' %(independent_var1)]
+                    columnindependent_var2 = data['%s' %(independent_var2)]
+                    columndependent_var = data['%s' %(dependent_var)]
+
+                    x = []
+                    x1 = list(columnindependent_var1)
+                    x2 = list(columnindependent_var2)
+                    lengthx1 = len(x1)
+                    for i in range(0, lengthx1):
+                        x.append([x1[i], x2[i]])
+                    print(x)
+                    y = list(columndependent_var)
+
+                    x = np.array(x)
                     y = np.array(y)
 
                     x = sm.add_constant(x)
